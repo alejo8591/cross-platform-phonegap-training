6b773d9d6a00b137f1ec1c8a6624db2544cbb53f
Alejandro Romero <alejo8591@gmail.com>
2014-01-13 16:21:57 -0500

    push "unidad-3" folder

---
+++ unidad-3/lab2/square.js
@@ -0,0 +1,22 @@
+var logic = require('./logic');
+
+exports.get = function(req, res){
+	res.writeHead(200, {
+		'Content-Type' : 'text/html'
+	});
+
+	res.end(
+		logic.page('Square', logic.navbar(), 
+			[
+				(!isNaN(req.a) ? 
+					('<p>{a} square = {sq}</p>'
+					 .replace('{a}', req.a)
+					 .replace('{sq}', req.a * req.a))
+					: ''), 
+				'<p>Insert number for calculate square</p>',
+				'<form name="square" action="/square" method="get">',
+				'of: <input type="text" name="a">',
+				'</form>'
+			].join('\n'))
+	);
+}
